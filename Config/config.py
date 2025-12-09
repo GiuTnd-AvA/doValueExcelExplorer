eb74eaa8f5d21ad23f6b6941b2cc4bf3d65c3830
ciro.andreano <ciro.andreano@avanade.com>
2025-12-09 14:36:40 +0100

    Aggiunta cartella PowershellScripts con all'interno lo script ps1 e l'eseguibile in Python che viene richiamato nel main

--- Config/config.py
+++ Config/config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Main.py paths
-POWERSHELL_SCRIPT_PATH = r'C:\Users\ciro.andreano\Desktop\doValueExcelExplorer\ExportMCode.ps1'
+POWERSHELL_SCRIPT_PATH = r'C:\Users\ciro.andreano\Desktop\doValueExcelExplorer\PowerShellScripts\ExportMCode.ps1'
 EXCEL_ROOT_PATH = r'C:\Users\ciro.andreano\Desktop\doValue'
 EXPORT_MCODE_PATH = r'C:\Users\ciro.andreano\Desktop\Export M Code'
 
