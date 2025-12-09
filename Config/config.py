d26ec245d778f54d15ded77352c9db6aa23d6a3e
giuseppe.tanda <giuseppe.tanda@avanade.com>
2025-12-09 15:18:49 +0100

    impostato il path automatico

--- Config/config.py
+++ Config/config.py
@@ -1,10 +1,12 @@
 # config.py
+import os
 
+user_folder = os.path.expanduser("~")
 # Main.py paths
-POWERSHELL_SCRIPT_PATH = r'C:\Users\ciro.andreano\Desktop\doValueExcelExplorer\PowerShellScripts\ExportMCode.ps1'
-EXCEL_ROOT_PATH = r'C:\Users\ciro.andreano\Desktop\doValue'
-EXPORT_MCODE_PATH = r'C:\Users\ciro.andreano\Desktop\Export M Code'
+POWERSHELL_SCRIPT_PATH = rf'{user_folder}\Desktop\doValueExcelExplorer\PowerShellScripts\ExportMCode.ps1'
+EXCEL_ROOT_PATH = rf'{user_folder}\Desktop\doValue'
+EXPORT_MCODE_PATH = rf'{user_folder}\Desktop\Export M Code'
 
 # extract_db_from_excel.py paths
-EXCEL_INPUT_PATH = r'C:\Users\ciro.andreano\Desktop\Connessioni Trovate.xlsx'
-EXCEL_OUTPUT_PATH = r'C:\Users\ciro.andreano\Desktop\Risultati_SQL.xlsx'
+EXCEL_INPUT_PATH = rf'{user_folder}\Desktop\Connessioni Trovate.xlsx'
+EXCEL_OUTPUT_PATH = rf'{user_folder}\Desktop\Risultati_SQL.xlsx'
