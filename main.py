c27d9e21eee519c19b0347d695431e09aaa3976f
giuseppe.tanda <giuseppe.tanda@avanade.com>
2025-12-09 12:59:12 +0100

    inserita variabile di ambiente per il percorso utente

---
+++ main.py
@@ -0,0 +1,39 @@
+# from Excecute_Power_Shell_Script import ExecPsCode as ps
+from BusinessLogic.Business_Logic import BusinessLogic as bl
+from Report.Excel_Writer import ExcelWriter as ew
+import os
+
+
+user_folder = os.path.expanduser("~")
+run_ps = ps(rf'{user_folder}\Desktop\doValue\doValueExcelExplorer\ExportMCode.ps1')
+
+return_code, output, error = run_ps.run()
+
+if return_code == 0:
+    print("PowerShell script executed successfully.")
+    print("Output:")
+    print(output)
+else:
+    print("PowerShell script execution failed.")
+    print("Error:")
+    print(error)
+
+bl_obj = bl(rf'{user_folder}\Desktop\doValue', rf'{user_folder}\Desktop\doValue\Export M Code')
+
+aggregated_info = bl_obj.get_aggregated_info()
+
+columns = ['File_Name',
+           'Creatore_file',
+           'Ultimo_modificatore_file',
+           'Data_creazione_file',
+           'Data_ultima_modifica_file',
+           'Collegamento_esterno',
+           'Source',
+           'Server',
+           'Database',
+           'Schema',
+           'Table']
+
+stampa_report_connessioni = ew(rf'{user_folder}\Desktop','Report_Connessioni.xlsx')
+stampa_report_connessioni.write_excel(columns, aggregated_info)
+print("Report connessioni creato correttamente.")
