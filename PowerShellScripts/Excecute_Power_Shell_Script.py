810f2f57e42ea246ae33c4fff77b99b3efb08d12
ciro.andreano <ciro.andreano@avanade.com>
2025-12-10 11:04:15 +0100

    Ulteriori raffinamenti per config e variabili

--- PowerShellScripts/Excecute_Power_Shell_Script.py
+++ PowerShellScripts/Excecute_Power_Shell_Script.py
@@ -1,24 +1,25 @@
 class ExecPsCode():
-
-    #costruttore che prende il path del file power shell
-    def __init__(self, power_shell_file_path):
+    # Costruttore che prende il path del file PowerShell e i parametri di percorso
+    def __init__(self, power_shell_file_path, folder, export_mcode_path):
         self.power_shell_file_path = power_shell_file_path
-    
+        self.folder = folder
+        self.export_mcode_path = export_mcode_path
+
     def run(self):
         import subprocess
-
-        # Execute the PowerShell script
+        # Execute the PowerShell script con parametri
         process = subprocess.Popen(
-            ["powershell", "-ExecutionPolicy", "Bypass", "-File", self.power_shell_file_path],
+            [
+                "powershell", "-ExecutionPolicy", "Bypass", "-File", self.power_shell_file_path,
+                "-folder", self.folder,
+                "-exportFolder", self.export_mcode_path
+            ],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
-
         # Capture the output and errors
         output, error = process.communicate()
-
         if process.returncode != 0:
             raise Exception(f"PowerShell script failed with error: {error.strip()}")
-
         return process.returncode, output.strip(), error.strip()
