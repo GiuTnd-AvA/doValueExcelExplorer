632db10f03466156087d810c4d0688cf2ad07ac1
ciro.andreano <ciro.andreano@avanade.com>
2025-12-10 14:54:49 +0100

    modifiche a config.ps1 config.py e main.py (adesso le variabili basta configurarle una volta nei due config ed eseguire il main)

--- Config/config.py
+++ Config/config.py
@@ -7,9 +7,6 @@ POWERSHELL_SCRIPT_PATH = rf'{user_folder}\Desktop\doValueExcelExplorer\PowerShel
 EXCEL_ROOT_PATH = rf'{user_folder}\Desktop\doValue'
 EXPORT_MCODE_PATH = rf'{user_folder}\Desktop\Export M Code'
 
-# PowerShell script paths
-FOLDER = rf'{user_folder}\Desktop\doValue'  # Cartella radice
-
 # extract_db_from_excel.py paths
 EXCEL_INPUT_PATH = rf'{user_folder}\Desktop\Connessioni Trovate.xlsx'
 EXCEL_OUTPUT_PATH = rf'{user_folder}\Desktop'
