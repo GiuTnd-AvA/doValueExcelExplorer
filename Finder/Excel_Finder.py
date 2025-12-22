166972b7c2dcf7922b09bd284b9f2161a5d59450
ciro.andreano <ciro.andreano@avanade.com>
2025-12-22 18:36:28 +0100

    rimossa deduplicazione e punta anche ai file .xlsm, .xls e .xlsb

--- Finder/Excel_Finder.py
+++ Finder/Excel_Finder.py
@@ -1,8 +1,21 @@
+
+import os
 from Finder.IFinder import IFinder
 
 class ExcelFinder(IFinder):
+    EXTENSIONS = ['.xls', '.xlsm', '.xlsx', '.xlsb']
 
-    EXTENSION = '.xlsx'
+    def file_finder(self) -> list[str]:
+        found_files: list[str] = []
+        try:
+            for root, dirs, files in os.walk(self.root_path):
+                for f in files:
+                    if any(f.endswith(ext) for ext in self.EXTENSIONS) and not f.startswith("~$"):
+                        full_path = os.path.join(root, f)
+                        found_files.append(full_path)
+        except Exception as e:
+            print(f"Errore nella ricerca dei file Excel: {e}")
+        return found_files
 
                     
 
